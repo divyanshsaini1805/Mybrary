dfaf6040b0feaa5df2d294cfb578600bde9d9743
Divyansh Saini <divyanshsaini1805@gmail.com>
2020-12-04 01:20:01 +0530

    2nd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 if(process.env != "production")
 {
-    require('dotenv').config();
+    require('dotenv').config({path:'.env'});
 }
 
 const express = require("express")
@@ -11,10 +11,23 @@ const indexRouter = require('./routes/index.js')
 
 app.set('view engine','ejs')
 
+
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout.ejs')
 app.use(expressLayouts)
 app.use('static',express.static('public'))
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://DIVYANSH:<yl7GpHDqKD6WtB0c>@cluster0.mvrxk.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL ,  { useNewUrlParser: true , useUnifiedTopology: true})
@@ -26,8 +39,6 @@ db.once('open', () => console.log("connected to mongoose"))
 
 
 
-
 app.use('/',indexRouter)
-
 app.listen(process.env.PORT || 80)
 
